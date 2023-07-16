0023adbed884dc4fdfe2af590de81e5a091b3c09
milpalabras <sergio.milpalabras@gmail.com>
2023-07-16 19:18:45 -0300

    refactor using gneric classes

--- api/views.py
+++ api/views.py
@@ -7,32 +7,12 @@ from api.serializers import AccountSerializer, CategorySerializer, RecordsSerial
 
 #views for accounts model
 
-class AccountList(mixins.ListModelMixin,
-                  mixins.CreateModelMixin,
-                  generics.GenericAPIView):
+class AccountList(generics.ListCreateAPIView):
     queryset = Account.objects.all()
     serializer_class = AccountSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self,request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-    
-class AccountDetail(mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin,
-                    generics.GenericAPIView):
+class AccountDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Account.objects.all()
     serializer_class = AccountSerializer
-    
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-    
-    def put(self,request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-    
-    def delete(self,request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
 
     
