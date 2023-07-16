82f019bc97b00a9dfc78f9c03a443bc1201b4f00
milpalabras <sergio.milpalabras@gmail.com>
2023-07-16 19:31:03 -0300

    Refactor generic classes views

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,12 @@ from api import views
 urlpatterns = [
     path('accounts/', views.AccountList.as_view()),
     path('accounts/<int:pk>/', views.AccountDetail.as_view()),
+    path('categories/', views.CategoryList.as_view()),
+    path('categories/<int:pk>/', views.CategoryDetail.as_view()),
+    path('records/', views.RecordsList.as_view()),
+    path('records/<int:pk>/', views.RecordsDetail.as_view()),
+    path('methodofpayment/', views.MethodOfPaymentList.as_view()),
+    path('methodofpayment/<int:pk>/', views.MethodOfPaymentDetail.as_view()),    
 ]
 
 # Using format suffixes gives us URLs that explicitly refer to a given format
--- api/views.py
+++ api/views.py
@@ -15,4 +15,34 @@ class AccountDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Account.objects.all()
     serializer_class = AccountSerializer
 
-    
+
+#views for category model
+
+class CategoryList(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class CategoryDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+#views for records model
+
+class RecordsList(generics.ListCreateAPIView):
+    queryset = Records.objects.all()
+    serializer_class = RecordsSerializer
+
+class RecordsDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Records.objects.all()
+    serializer_class = RecordsSerializer
+
+#views for method of payment model
+
+class MethodOfPaymentList(generics.ListCreateAPIView):
+    queryset = MethodOfPayment.objects.all()
+    serializer_class = MethodOfPaymentSerializer
+
+class MethodOfPaymentDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = MethodOfPayment.objects.all()
+    serializer_class = MethodOfPaymentSerializer
+
